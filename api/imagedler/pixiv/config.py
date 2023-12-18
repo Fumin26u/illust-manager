346b1be49d9819faff89e3346a075344b65b104f
Fumiya0719 <75408448+Fumiya0719@users.noreply.github.com>
2023-12-18 19:51:16 +0900

    imagedler関連の必要そうなファイルをとりあえず移動

---
+++ api/imagedler/pixiv/config.py
@@ -0,0 +1,28 @@
+# pixivpy token
+GET_CODE = "QF2i6B8ryq2J_RfKDicJ6Y6Jnkt0UQHX_vtiaGg5MMc"
+ACCESS_TOKEN = "8a6RsZmNFcsuDGgPfIesoJ2EQy8z7qhF7RCSu0u3HAE"
+REFRESH_TOKEN = "C3ol1zi9CDXshCwXHMGrOwMYegdyzXpeNJwyoAmpIz0"
+EXPIRES_IN = 3600
+
+# タグフィルター設定時のタグ設定
+# タグリスト：配列内のタグが1つでも含まれていたら追加
+TAG_LIST = [
+    'オリジナル',
+    'アイドルマスターミリオンライブ!',
+    # 'アイドルマスターシャイニーカラーズ',
+    'ご注文はうさぎですか?',
+    'アズールレーン',
+    # 'ブルーアーカイブ',
+    'きんいろモザイク',
+    '同人',
+    '創作',
+    '原神',
+    'GenshinImpact',
+    '超次元ゲイムネプテューヌ'
+    # '女の子',
+]
+
+# 除外タグリスト：配列内のタグが1つでも含まれていたら除外
+EX_TAG_LIST = [
+    'VTuber',
+]
---
+++ api/imagedler/twitter/twitterPassword.py
@@ -0,0 +1,2 @@
+# パスワードをここに入力
+USER_PASSWORD = 'ktH1026ttiwmnk'
