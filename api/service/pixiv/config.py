d9a27f90ac4e57a7f325ae6cfcdaea3b4bd2e698
Fumiya <75408448+Fumin26u@users.noreply.github.com>
2024-07-06 22:02:56 +0900

    MVC構造に置き換えてとりあえずUser周りを作った

---
+++ api/service/pixiv/config.py
@@ -0,0 +1,11 @@
+# pixivpy token
+GET_CODE = "yi7wxzfMGDuzGSF1kT-z6hU-2vY-79KgZMAaJE3Cf08"
+ACCESS_TOKEN = "gds1JNaMtGbzXbABF4jnsmhFL0LMesd9lo_TvwNX_9A"
+REFRESH_TOKEN = "C3ol1zi9CDXshCwXHMGrOwMYegdyzXpeNJwyoAmpIz0"
+EXPIRES_IN = 3600
+
+# タグフィルター設定時のタグ設定
+# タグリスト：配列内のタグが1つでも含まれていたら追加
+TAG_LIST = []
+# 除外タグリスト：配列内のタグが1つでも含まれていたら除外
+EX_TAG_LIST = []
